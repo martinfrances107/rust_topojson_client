05f73c486c8b741f8f58d148f30d25eb4b5dea40
Martin <martinfrances107@hotmail.com>
2021-11-09 11:23:17 +0000

    * Using predefined structs in the topojson crate simplifies the transform function generator. * Added intial documentation boiler plate,

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,22 @@
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
 #![allow(clippy::many_single_char_names)]
+//! A port of [topojson/topojson-client](<https://github.com/topojson/topojson-client>).
+//!
+//! Manipulate TopoJSON, such as to merge shapes, and convert it back to GeoJSON.
+//!
+//! <hr>
+//!
+//! Repository [rust_topojson_client](<https://github.com/martinfrances107/rust_topojson_client>)
 
 extern crate derivative;
+extern crate topojson;
 
-// /// function feature() and various From implementations.
-// mod feature;
+/// function feature() and various From implementations.
+mod feature;
+
 /// function reverse() and unit tests.
 mod reverse;
-/// function transform()  helper type TransformFn
+
+/// function generate, helper type TransformFn and unit tests.
 mod transform;
--- src/transform.rs
+++ src/transform.rs
@@ -1,74 +1,46 @@
-use serde_json::Value;
+use topojson::Position;
+use topojson::TransformParams;
 
-pub type TransformFn = Box<dyn FnMut(Vec<i32>, Option<i32>) -> Vec<i32>>;
+pub type TransformFn = Box<dyn FnMut(Position, Option<i32>) -> Vec<f64>>;
 
-pub fn transform(transform: Option<Value>) -> TransformFn {
-    match transform {
-        None => {
-            // identity transform
-            Box::new(|x: Vec<i32>, _i32| x)
-        }
-        Some(t) => {
-            let mut x0 = 0;
-            let mut y0 = 0;
-            let kx: i32 = t
-                .pointer("/scale/0")
-                .expect("must contain a scale array")
-                .as_i64()
-                .expect("must be an int")
-                .try_into()
-                .expect("cannot reduce to 32 bits.");
-            let ky: i32 = t
-                .pointer("/scale/1")
-                .expect("must contain a scale array")
-                .as_i64()
-                .expect("must be an int")
-                .try_into()
-                .expect("cannot reduce to 32 bits.");
-            let dx: i32 = t
-                .pointer("/translate/0")
-                .expect("must contain a translate array")
-                .as_i64()
-                .expect("must be an int")
-                .try_into()
-                .expect("cannot reduce to 32 bits.");
-            let dy: i32 = t
-                .pointer("/translate/1")
-                .expect("must contain a translate array")
-                .as_i64()
-                .expect("must be an int")
-                .try_into()
-                .expect("cannot reduce to 32 bits.");
+/// Given a set of transform parmaters return a tranformfunction that transform
+/// position into a Vec<f64>
+pub fn generate(tp: TransformParams) -> TransformFn {
+    let mut x0 = 0_f64;
+    let mut y0 = 0_f64;
+    let kx = tp.scale[0];
+    let ky = tp.scale[1];
 
-            Box::new(move |input, i| -> Vec<i32> {
-                match i {
-                    Some(i) => {
-                        if i == 0 {
-                            x0 = 0;
-                            y0 = 0;
-                        }
-                    }
-                    None => {
-                        x0 = 0;
-                        y0 = 0;
-                    }
+    let dx = tp.translate[0];
+    let dy = tp.translate[1];
+
+    Box::new(move |input: Position, i| -> Vec<f64> {
+        match i {
+            Some(i) => {
+                if i == 0 {
+                    x0 = 0_f64;
+                    y0 = 0_f64;
                 }
-
-                let mut j = 2;
-                let n = input.len();
-                let mut output = Vec::with_capacity(n);
-                x0 += input[0];
-                output.push(x0 * kx + dx);
-                y0 += input[1];
-                output.push(y0 * ky + dy);
-                while j < n {
-                    output.push(input[j]);
-                    j += 1;
-                }
-                output
-            })
+            }
+            None => {
+                x0 = 0_f64;
+                y0 = 0_f64;
+            }
         }
-    }
+
+        let mut j = 2;
+        let n = input.len();
+        let mut output = Vec::with_capacity(n);
+        x0 += input[0];
+        output.push(x0 * kx + dx);
+        y0 += input[1];
+        output.push(y0 * ky + dy);
+        while j < n {
+            output.push(input[j]);
+            j += 1;
+        }
+        output
+    })
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -77,8 +49,6 @@ mod transform_tests {
     extern crate pretty_assertions;
 
     use super::*;
-
-    use serde_json::json;
 
     // tape("topojson.transform(topology) returns the identity function if transform is undefined", function(test) {
     //   var transform = topojson.transform(null),
@@ -104,11 +74,12 @@ mod transform_tests {
     #[test]
     fn returns_a_point_transform_function() {
         println!("topojson.transform(topology) returns a point-transform function if transform is defined");
-        let mut transform = transform(Some(json!({
-            "scale": [2, 3],
-            "translate": [4,5]
-        })));
-        assert_eq!(transform(vec![6, 7], None), [16, 26]);
+        let mut transform = generate(TransformParams {
+            scale: [2_f64, 3_f64],
+            translate: [4_f64, 5_f64],
+        });
+
+        assert_eq!(transform(vec![6_f64, 7_f64], None), [16_f64, 26_f64]);
     }
 
     // This test does not need to be ported because rust handles mutability differently.
@@ -125,52 +96,55 @@ mod transform_tests {
     #[test]
     fn preserves_extra_dimensions() {
         println!("transform(point) preserves extra dimensions");
-        let mut transform = transform(Some(json!({
-            "scale": [2, 3],
-            "translate": [4,5]
-        })));
-        assert_eq!(transform(vec![6, 7, 42], None), [16, 26, 42]);
+        let mut transform = generate(TransformParams {
+            scale: [2_f64, 3_f64],
+            translate: [4_f64, 5_f64],
+        });
+        assert_eq!(
+            transform(vec![6_f64, 7_f64, 42_f64], None),
+            [16_f64, 26_f64, 42_f64]
+        );
     }
 
     #[test]
     fn transforms_individual_points() {
         println!("transform(point) transforms individual points");
-        let mut transform = transform(Some(json!({
-            "scale": [2, 3],
-            "translate": [4,5]
-        })));
-        assert_eq!(transform(vec![1, 2], None), [6, 11]);
-        assert_eq!(transform(vec![3, 4], None), [10, 17]);
-        assert_eq!(transform(vec![5, 6], None), [14, 23]);
+        let mut transform = generate(TransformParams {
+            scale: [2_f64, 3_f64],
+            translate: [4_f64, 5_f64],
+        });
+        assert_eq!(transform(vec![1_f64, 2_f64], None), [6_f64, 11_f64]);
+        assert_eq!(transform(vec![3_f64, 4_f64], None), [10_f64, 17_f64]);
+        assert_eq!(transform(vec![5_f64, 6_f64], None), [14_f64, 23_f64]);
     }
 
     #[test]
     fn transforms_delta_encoded_arcs() {
         println!("transform(point, index) transforms delta-encoded arcs");
-        let mut transform = transform(Some(json!({
-            "scale": [2, 3],
-            "translate": [4,5]
-        })));
-        assert_eq!(transform(vec![1, 2], Some(0)), [6, 11]);
-        assert_eq!(transform(vec![3, 4], Some(1)), [12, 23]);
-        assert_eq!(transform(vec![5, 6], Some(2)), [22, 41]);
-        assert_eq!(transform(vec![1, 2], Some(3)), [24, 47]);
-        assert_eq!(transform(vec![3, 4], Some(4)), [30, 59]);
-        assert_eq!(transform(vec![5, 6], Some(5)), [40, 77]);
+        let mut transform = generate(TransformParams {
+            scale: [2_f64, 3_f64],
+            translate: [4_f64, 5_f64],
+        });
+        assert_eq!(transform(vec![1_f64, 2_f64], Some(0)), [6_f64, 11_f64]);
+        assert_eq!(transform(vec![3_f64, 4_f64], Some(1)), [12_f64, 23_f64]);
+        assert_eq!(transform(vec![5_f64, 6_f64], Some(2)), [22_f64, 41_f64]);
+        assert_eq!(transform(vec![1_f64, 2_f64], Some(3)), [24_f64, 47_f64]);
+        assert_eq!(transform(vec![3_f64, 4_f64], Some(4)), [30_f64, 59_f64]);
+        assert_eq!(transform(vec![5_f64, 6_f64], Some(5)), [40_f64, 77_f64]);
     }
 
     #[test]
     fn transforms_mutliple_delta_encoded_arcs() {
         println!("transform(point, index) transforms delta-encoded arcs");
-        let mut transform = transform(Some(json!({
-            "scale": [2, 3],
-            "translate": [4,5]
-        })));
-        assert_eq!(transform(vec![1, 2], Some(0)), [6, 11]);
-        assert_eq!(transform(vec![3, 4], Some(1)), [12, 23]);
-        assert_eq!(transform(vec![5, 6], Some(2)), [22, 41]);
-        assert_eq!(transform(vec![1, 2], Some(0)), [6, 11]);
-        assert_eq!(transform(vec![3, 4], Some(1)), [12, 23]);
-        assert_eq!(transform(vec![5, 6], Some(2)), [22, 41]);
+        let mut transform = generate(TransformParams {
+            scale: [2_f64, 3_f64],
+            translate: [4_f64, 5_f64],
+        });
+        assert_eq!(transform(vec![1_f64, 2_f64], Some(0)), [6_f64, 11_f64]);
+        assert_eq!(transform(vec![3_f64, 4_f64], Some(1)), [12_f64, 23_f64]);
+        assert_eq!(transform(vec![5_f64, 6_f64], Some(2)), [22_f64, 41_f64]);
+        assert_eq!(transform(vec![1_f64, 2_f64], Some(0)), [6_f64, 11_f64]);
+        assert_eq!(transform(vec![3_f64, 4_f64], Some(1)), [12_f64, 23_f64]);
+        assert_eq!(transform(vec![5_f64, 6_f64], Some(2)), [22_f64, 41_f64]);
     }
 }
