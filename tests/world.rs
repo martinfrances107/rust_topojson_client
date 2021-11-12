5fcb44bc7be26845946f8de596dd3162fa8ce841
Martin <martinfrances107@hotmail.com>
2021-11-12 08:25:11 +0000

    Added countries test skeleton to show the bug.

--- tests/world.rs
+++ tests/world.rs
@@ -5,7 +5,7 @@ extern crate topojson;
 
 #[cfg(test)]
 mod world_test {
-    use std::env;
+
     use std::fs::File;
     use std::io::Read;
 
@@ -16,20 +16,16 @@ mod world_test {
     /// Asserts that a MultiPolygon object with 1428 polygons
     /// can be extracted from the "land" object within the map.
     #[test]
-    pub fn land_decode() {
+    pub fn object_decode() {
         let mut file =
             File::open("./tests/world-atlas/world/50m.json").expect("File did not open.");
         let mut contents = String::new();
         file.read_to_string(&mut contents)
             .expect("Could not read file.");
 
-        let topology: Topology = serde_json::from_str(&contents).expect("Faile to read as json.");
+        let topology: Topology = serde_json::from_str(&contents).expect("Failed to read as json.");
 
-        dbg!(&topology.transform);
-
-        let computed = Builder::<f64>::generate_from_name(&topology, &"land");
-
-        match computed {
+        match Builder::<f64>::generate_from_name(&topology, &"land") {
             Some(Geometry::GeometryCollection(GeometryCollection(v_geometry))) => {
                 assert_eq!(v_geometry.len(), 1);
                 match &v_geometry[0] {
@@ -37,7 +33,7 @@ mod world_test {
                         assert_eq!(mp.0.len(), 1428_usize);
                     }
                     _ => {
-                        assert!(false, "Faile to decode Multipoloygon")
+                        assert!(false, "Failed to decode Multipoloygon")
                     }
                 }
             }
@@ -45,5 +41,23 @@ mod world_test {
                 assert!(false, "failed to extract a vector of geometries");
             }
         };
+
+        // TODO: This fails to parse .. overflows in reverse()
+        // match Builder::<f64>::generate_from_name(&topology, &"countries") {
+        //     Some(Geometry::GeometryCollection(GeometryCollection(v_geometry))) => {
+        //         assert_eq!(v_geometry.len(), 241);
+        //         match &v_geometry[0] {
+        //             Geometry::MultiPolygon(mp) => {
+        //                 assert_eq!(mp.0.len(), 1428_usize);
+        //             }
+        //             _ => {
+        //                 assert!(false, "Faile to decode Multipoloygon")
+        //             }
+        //         }
+        //     }
+        //     _ => {
+        //         assert!(false, "failed to extract a vector of geometries");
+        //     }
+        // };
     }
 }
