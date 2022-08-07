1928ed19642ad898ee0da8f56fcd547262d522db
Martin <martinfrances107@hotmail.com>
2022-08-07 16:50:16 +0100

    Added tests to bisect -- these tests do not exist in javascript.

--- lib/src/bisect.rs
+++ lib/src/bisect.rs
@@ -1,3 +1,12 @@
+/// Returns the bisection index for a slice of arc indexes.
+///
+/// Performs binary search.
+///
+/// # Arguments
+///
+/// * 'a' - A slice of arc indexes.
+/// * 'x' - The threshold value use to separate into two halves.
+///
 pub fn bisect(a: &[i32], x: i32) -> usize {
     let mut lo = 0;
     let mut hi = a.len();
@@ -11,4 +20,18 @@ pub fn bisect(a: &[i32], x: i32) -> usize {
         }
     }
     lo
+}
+
+#[cfg(not(tarpaulin_include))]
+#[cfg(test)]
+mod bisect_test {
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn simple(){
+        assert_eq!(bisect(&[0, 2, 3, 4], 4), 3);
+        assert_eq!(bisect(&[1, 2, 3, 4], 3), 2);
+    }
 }
