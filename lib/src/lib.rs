bbfb08a158b45a599f0b847bba6509fe60c1b351
Martin <martinfrances107@hotmail.com>
2022-08-17 08:01:56 +0100

    Minor: Added testing to show negative numbers are tranlate()'ed to the ones compliement version.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -33,13 +33,36 @@ mod stitch;
 /// function generate, helper type TransformFn and unit tests.
 mod transform;
 
-// Translate ARC indexes which are signed offsets
-// into absolute positions in the array.
+/// Translate ARC indexes.
+///
+/// "A negative arc index indicates that the arc at the ones’ complement of the index must be reversed
+/// to reconstruct the geometry: -1 refers to the reversed first arc, -2 refers to the reversed second arc,
+/// and so on."
+///
+/// [source: 2.1.4. Arc Indexes](https://github.com/topojson/topojson-specification#214-arc-indexes)
 #[inline]
 fn translate(arc: i32) -> usize {
     if arc < 0 {
         !arc as usize
     } else {
         arc as usize
+    }
+}
+
+#[cfg(not(tarpaulin_include))]
+#[cfg(test)]
+mod translate_tests {
+
+    use super::translate;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_double_zero() {
+        let input = [-2, -1, 0, 1, 2];
+        let mut output = [usize::MIN; 5];
+        for (i, arc) in input.iter().enumerate() {
+            output[i] = translate(*arc);
+        }
+        assert_eq!(output, [1, 0, 0, 1, 2]);
     }
 }
